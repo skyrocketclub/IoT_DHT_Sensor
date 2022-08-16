20979eb41dc5a15dccad4a64a21f7c495ff301df
kitis <68389270+skyrocketclub@users.noreply.github.com>
2022-08-16 09:50:41 +0100

    Connected to ThingSpeak and sending the readings to the server every 20 seconds

--- README.md
+++ README.md
@@ -9,5 +9,14 @@ STEPS I TOOK TO GET THE ESP8266 TO START WORKING
 4. I went back to the PlatformIO / (or Arduino) and I set the COM port to COM7. (might be different on your system)
 5. Wipe your tears, Redemption has come!
 
+
+STEPS TO CONNECT THE PROJECT TO THE THINKSPEAK SERVER
+1. Install the Thinkspeak library by Mathworks (which should be available if you clone this repo)
+2. Open an account at https://thingspeak.com/
+3. Create a new channel - Name it after the project you are working on prefarably
+4. Enter the fields - corresponding to the parameters you want to send and track on the server...
+5. take note of you channel id & your writeAPIkey
+6. The rest is history.
+
 THE SCHEMATICS OF THE PROJECT
 ![Schematics](https://user-images.githubusercontent.com/68389270/184555289-03e66266-4936-48e9-8f29-5151d27f3ceb.jpg)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 // #include <Arduino.h>
 #include "DHT.h"
+#include <ESP8266WiFi.h>
+#include "ThingSpeak.h"
 
 //D2 on NodeMCU is GPIO 4
 #define DHTPIN 4 
 
-#include <ESP8266WiFi.h>
-
 const char* ssid = "Incognito";
 const char* password = "biscuits";
+
+unsigned long myChannelNumber = 1832709;
+const char * myWriteAPIKey = "VQIG4M1SVDOMM9HH";
+const char* server = "api.thingspeak.com";
+
+//Initialize the client library...
+WiFiClient client;
 
 //the type of sensor we are using
 // define DHTTYPE DHT!!
@@ -16,12 +23,22 @@ const char* password = "biscuits";
 //Instance of DHT takes two arguments
 DHT dht(DHTPIN,DHTTYPE);
 
+void updateThingSpeak(float t, float h){
+  ThingSpeak.setField(1,t);
+  ThingSpeak.setField(2,h);
+  ThingSpeak.writeFields(myChannelNumber,myWriteAPIKey);
+  delay(20*1000); //ThingSpeak recommends a delay of 20 seconds
+}
+
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);
-  WiFi.begin(ssid,password);
+  delay(1000);
   dht.begin();
+  ThingSpeak.begin(client);
+  WiFi.begin(ssid,password);
+  
 
   //Connecting the board to the wifi
    while(WiFi.status() != WL_CONNECTED){
@@ -72,5 +89,9 @@ void loop() {
   Serial.print(" *C ");
   Serial.print(hif);
   Serial.println(" *F");
+
+  if(client.connect(server,80)){
+    updateThingSpeak(t,h);
+  }
   
 }
