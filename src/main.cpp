e51eac0bcb1a428061064832c8ce289baadd0944
kitis <68389270+skyrocketclub@users.noreply.github.com>
2022-08-26 10:04:29 +0100

    EEPROM Implemented

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,12 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <time.h>
+//Contains all the credentials and Information related to AWS
 #include "secrets.h"
+//Neededfor implementing EEPROM
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
 
 
 //D2 on NodeMCU is GPIO 4
@@ -14,6 +19,16 @@
 #define DHTTYPE DHT11 
 //Instance of DHT takes two arguments
 DHT dht(DHTPIN,DHTTYPE);
+
+//Variables
+int i = 0;
+int statusCode;
+const char* ssid = "text";
+const char* passphrase = "text";
+String st;
+String content;
+ESP8266WebServer server(80); //port 80
+
 
 // const char* ssid = "Incognito";
 // const char* password = "biscuits";
@@ -48,6 +63,7 @@ const long interval = 5000;
 
 WiFiClientSecure net;
 
+//For the AWS Certificates 
 BearSSL::X509List cert(cacert);
 BearSSL::X509List client_crt(client_cert);
 BearSSL::PrivateKey key(privkey);
@@ -57,6 +73,13 @@ PubSubClient client(net);
 time_t now;
 time_t nowish = 1510592825;
 
+//Function declarations
+bool testWiFi(void);
+void launchWeb(void);
+void setupAP(void);
+void createWebServer();
+
+//Used to set the time
 void NTPConnect(void){
   Serial.print("Setting time using SNTP");
   configTime(TIME_ZONE * 3600, 0 * 3600, "pool.ntp.org", "time.nist.gov");
@@ -73,19 +96,26 @@ void NTPConnect(void){
   Serial.print(asctime(&timeinfo));
 }
 
+//Called when the brokar publishes data to a topic our project suscribes to
 void messageReceived(char *topic, byte *payload, unsigned int length){
   Serial.print("Received [");
   Serial.print(topic);
   Serial.print("]: ");
   for (int i = 0; i< length; i++){
+    //The payload is the message that the broker sends
     Serial.print((char)payload[i]);
   }
   Serial.println();
 }
 
+//This function helps to connect to AWS IoT
 void connectAWS(){
   delay(3000);
-  WiFi.mode(WIFI_STA);
+
+//The default WiFi COnnection
+  /*
+  WiFi.mode(WIFI_STA); // wifi is in station mode - The Default
+  //it firsts tries to connect to you WiFi
   WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
 
   Serial.println(String("Attempting to connect to SSID: ") + String(WIFI_SSID));
@@ -95,7 +125,61 @@ void connectAWS(){
     Serial.print(".");
     delay(1000);
   }
+*/
+//Implementing EEPROM
+Serial.println("Disconnecting previously connected WiFi");
+WiFi.disconnect();
+EEPROM.begin(512);
+delay(10);
+pinMode(LED_BUILTIN,OUTPUT);
+Serial.println();
+Serial.println();
+Serial.println("Startup");
+
+//Reading EEPROM Memory FOR SSID & PASS
+Serial.print("Reading EEPROM SSID");
+String esid;
+for(int i =0; i<32; i++){
+  esid += char(EEPROM.read(i));
+}
+Serial.println();
+Serial.print("SSID: ");
+Serial.print(esid);
+Serial.println("reading EEPROM pass");
+
+String epass = "";
+for(int i = 32; i<96; i++){
+  epass += char(EEPROM.read(i));
+}
+Serial.print("PASS: ");
+Serial.println(epass);
+
+//You try to connect to WiFi with the details you got from EEPROM Memory
+WiFi.begin(esid.c_str(),epass.c_str());
+if(testWiFi()){
+  //WiFi in Station Mode
+  Serial.println("Succesfully COnnected!!!");
+  return;
+}
+else{
+//TIme to turn the ESP8266 to an AP - Access Point
+  Serial.println("Turning the Hotspot On");
+  launchWeb();
+  setupAP(); 
+}
+
+Serial.println();
+Serial.println("Waiting...");
+
+while((WiFi.status() != WL_CONNECTED)){
+  Serial.print(".");
+  delay(100);
+  server.handleClient();
+}
+
+  //When the wifi is connected, then the time is set
   NTPConnect();
+  //The certifications are consumed in SSL format defined above...
   net.setTrustAnchors(&cert);
   net.setClientRSACert(&client_crt, &key);
 
@@ -104,6 +188,7 @@ void connectAWS(){
 
   Serial.println("Connecting to AWS IOT");
 
+  //Make sure Thing name is same as that in AWS
   while(!client.connect(THINGNAME)){
     Serial.print(".");
     delay(1000);
@@ -119,6 +204,7 @@ void connectAWS(){
   Serial.println("AWS IoT Connected!");
 }
 
+//Here your message is published, the pattern of using JSON is important
 void publishMessage(){
   StaticJsonDocument<200> doc;
   doc["time"] = millis();
@@ -126,8 +212,152 @@ void publishMessage(){
   doc["temperature"]=t;
   char jsonBuffer[512];
   serializeJson(doc,jsonBuffer);
-
+// the topic . the jsonBuffer passed as parameters
   client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
+}
+
+//This is what tells us that it's time to switch to AP
+bool testWiFi(void){
+  int c =0;
+  Serial.println("Waiting for WiFi to connect");
+  while(c<20){
+    if(WiFi.status() == WL_CONNECTED){
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("COnnection timed out, opening AP");
+  return false;
+}
+
+void launchWeb(void){
+  Serial.println("");
+  if(WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("WiFi connected");
+  }
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  //start the server
+}
+
+void setupAP(void){
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if(n == 0){
+    Serial.println("no networks found");
+  }
+  else{
+    Serial.print(n);
+    Serial.println(" networks found:");
+    for(int i = 0; i< n; i++){
+      //Print SSID and RSSI for each network found
+      Serial.print(i+1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(") ");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for(int i = 0; i<n; i++){
+    //Print the SSID and RSSI for each network found
+    st+= "<li>";
+    st+= WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+
+    st +=") ";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("how2electronics", "");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+
+void createWebServer()
+{
+ {
+    server.on("/", []() {
+ 
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+ 
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+ 
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+ 
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+ 
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+        ESP.reset();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+ 
+    });
+  } 
 }
 
 void setup() {
@@ -136,6 +366,8 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
   dht.begin();
+  server.begin();
+  connectAWS();
   // ThingSpeak.begin(client);
   // WiFi.begin(ssid,password);
   
